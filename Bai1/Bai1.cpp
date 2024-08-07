8c28b009e5ec5770f27ee3ebd8aa708fa4c2f7d5
Dinh <thanhvn1245@gmail.com>
2024-08-07 14:42:30 +0700

    Finish Bai01.3

--- Bai1/Bai1.cpp
+++ Bai1/Bai1.cpp
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int CoGhiHoa(const char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+        if (isupper(str[i])) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
     char name[] = "Nguyen Tran Dinh Thanh";
     printf("%s\n", name);
+
+    if (CoGhiHoa(name)) {
+        printf("Chuoi co chua ki tu hoa.\n");
+    } else {
+        printf("Chuoi khong chua ki tu hoa.\n");
+    }
+
     return 0;
 }
 
