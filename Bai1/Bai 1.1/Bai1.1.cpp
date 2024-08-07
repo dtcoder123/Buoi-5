d5925ec815adc3643eb7cd2c9815812fdfd9ca11
Dinh <thanhvn1245@gmail.com>
2024-08-07 14:52:14 +0700

    Commit lai Bai01.1+Bai01.2+Bai01.3

---
+++ Bai1/Bai 1.1/Bai1.1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char name[] = "Nguyen Tran Dinh Thanh";
+    return 0;
+}
+
---
+++ Bai1/Bai 1.2/Bai1.2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main() {
+    char name[] = "Nguyen Tran Dinh Thanh";
+    printf("%s\n", name);
+    return 0;
+}
+
